bf1d83fb119fdd255fc307245c68c4f4fef77e31
Diego <diegojrq@gmail.com>
2023-09-04 11:28:45 -0300

    Adicionando sample de api request

--- front/src/layouts/default/AppBar.vue
+++ front/src/layouts/default/AppBar.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app-bar title="Application bar">
-      <RouterLink to="/">Home</RouterLink>      
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/books">Books</RouterLink>
       <RouterLink to="/login">Login</RouterLink>
       <RouterLink to="/register">Register</RouterLink>
   </v-app-bar>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -44,6 +44,20 @@ const routes = [
       },
     ],
   },
+  {
+    path: '/books',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'BookList',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/book/BookList.vue'),
+      },
+    ],
+  },
 ]
 
 const router = createRouter({
